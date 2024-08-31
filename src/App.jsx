4ea640e1766bbf8a7e58e282ad139fe254adb020
Sandeeb Adhikari <sa2734@njit.edu>
2024-08-31 16:37:27 -0400

    made changes on different file

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,35 @@
-import React, { useState, useEffect } from "react";
-import Home from "./pages/Home";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const [backgroundClass, setBackgroundClass] = useState("");
-
-  const handleBackgroundClassChange = (newClass) => {
-    setBackgroundClass(newClass);
-  };
-
-  useEffect(() => {
-    console.log("Background class updated:", backgroundClass);
-  }, [backgroundClass]);
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className={`min-h-screen ${backgroundClass}`}>
-        <div className="background-overlay"></div> {/* Apply overlay here */}
-        <Home onBackgroundClassChange={handleBackgroundClassChange} />
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
